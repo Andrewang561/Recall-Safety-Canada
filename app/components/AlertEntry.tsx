80548ec4291c8baf22428cf1d88ed7f343b8fa08
andrewwang <andrewang561@hotmail.com>
2025-10-04 20:37:38 -0700

    Fixed formatting of AlertEntry component.

--- app/components/AlertEntry.tsx
+++ app/components/AlertEntry.tsx
@@ -4,14 +4,13 @@ import { recallData } from '../type/recall';
 export function AlertEntry({ data }: {data: recallData}) {
     const handlePress = () => {
         Linking.openURL(data.link);
-
     };
 
     return <Pressable style = {styles.container} onPress={ handlePress }>
         <Image source = {require('../../assets/images/food-icon.png')}  style = {styles.img}></Image>
-        <Text style = {styles.title}>{data.title}</Text>
-        <Text style = {styles.body}>{data.product}</Text>
-        <Text style = {styles.body}>{data.issue}</Text>
+        <Text style = {styles.title} numberOfLines={2} ellipsizeMode="tail">{data.title}</Text>
+        <Text style = {styles.product } numberOfLines={1} ellipsizeMode="tail">{data.product}</Text>
+        <Text style = {styles.issue} numberOfLines={2} ellipsizeMode="tail">{data.issue}</Text>
     </Pressable>
 }
 
@@ -21,21 +20,34 @@ const styles = StyleSheet.create({
         height: 200,
         backgroundColor: '#ffffff',
         position: 'relative',
+        borderWidth: 3,
+        borderColor: 'black',
+        borderRadius: 40,
+        marginBottom: 10,
+        left: 5,
     },
     title: {
-        fontSize: 28,
+        fontSize: 25,
         position: 'absolute',
-        top: 15,
-        left: 117,
-        width: 331,
+        top: 25,
+        left: 140,
+        width: 290,
         height: 85,
         textAlign: 'center',
     },
-    body: {
+    product: {
         fontSize: 15,
         position: 'absolute',
-        top: 105,
-        left: 181,
+        top: 90,
+        left: 210,
+        width: 220,
+        height: 172,
+    },
+    issue: {
+        fontSize: 15,
+        position: 'absolute',
+        top: 120,
+        left: 210,
         width: 220,
         height: 172,
     },
